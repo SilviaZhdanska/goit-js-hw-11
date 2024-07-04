1a68fd090c3223bccf02a34c37f20bfadfc4f98d
Silvia Zhdanska <silviaalbert1992@gmail.com>
2024-07-04 16:46:10 +0300

    some changes

---
+++ src/main.js
@@ -0,0 +1,10 @@
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+// Описаний у документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const API_KEY = 44780398 - b9dbe1b89370a8f5261d05d93;
