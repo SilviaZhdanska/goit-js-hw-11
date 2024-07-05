01e918ba8f1a570e78c4fae9ef8aee0011266307
Silvia Zhdanska <silviaalbert1992@gmail.com>
2024-07-05 22:47:59 +0300

    fast ready

--- src/main.js
+++ src/main.js
@@ -1,8 +1,43 @@
-// Описаний у документації
-import iziToast from 'izitoast';
-// Додатковий імпорт стилів
-import 'izitoast/dist/css/iziToast.min.css';
-// Описаний у документації
-import SimpleLightbox from 'simplelightbox';
-// Додатковий імпорт стилів
-import 'simplelightbox/dist/simple-lightbox.min.css';
+import { getPicturesByQuery } from './js/pixabay-api.js';
+import {
+  renderGallery,
+  showNoResultsMessage,
+  showFetchErrorMessage,
+} from './js/render-functions.js';
+
+const searchForm = document.querySelector('.search-form');
+const loader = document.querySelector('.loader');
+
+loader.style.display = 'none';
+
+searchForm.addEventListener('submit', handleSearch);
+
+function handleSearch(event) {
+  event.preventDefault();
+
+  const form = event.currentTarget;
+  const queryValue = form.elements.query.value.trim().toLowerCase();
+
+  if (!queryValue) {
+    return;
+  }
+
+  loader.style.display = 'inline-block';
+
+  getPicturesByQuery(queryValue)
+    .then(data => {
+      loader.style.display = 'none';
+
+      if (data.hits.length === 0) {
+        showNoResultsMessage();
+      } else {
+        renderGallery(data.hits);
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      loader.style.display = 'none';
+      showFetchErrorMessage();
+    })
+    .finally(() => form.reset());
+}
